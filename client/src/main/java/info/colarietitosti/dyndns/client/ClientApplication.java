64d096064755500b9da2a1c21b8ee6561087cdea
Andrea Colarieti Tosti <a.colarietitosti@googlemail.com>
2019-11-19 12:46:56 +0100

    Moved Service on Server

--- client/src/main/java/info/colarietitosti/dyndns/client/ClientApplication.java
+++ client/src/main/java/info/colarietitosti/dyndns/client/ClientApplication.java
@@ -9,9 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class ClientApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
-
 }
