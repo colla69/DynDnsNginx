1e0f853252ce5d80e0347feb96e079f35b7fb1bf
Andrea Colarieti Tosti <a.colarietitosti@googlemail.com>
2019-11-19 14:05:33 +0100

    test

--- server/src/main/java/info/colarititosti/dyndns/TemplateConfig.java
+++ server/src/main/java/info/colarititosti/dyndns/TemplateConfig.java
@@ -1,11 +1,9 @@
 package info.colarititosti.dyndns;
 
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -14,27 +12,30 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Component
 public class TemplateConfig {
 
+@PostConstruct
+public void test(){
+    TemplateConfig.saveFilledTemplates("dfs");
+}
 
     public static void saveFilledTemplates(String ip){
         try {
             String current = new java.io.File( "." ).getCanonicalPath();
             List<String> templates = getTemplates(current);
 
-            String template = templates.get(0);
-            Path path = Paths.get(template);
-            Charset charset = StandardCharsets.UTF_8;
-            String content = new String(Files.readAllBytes(path), charset);
-            content = content.replaceAll("\\[(.*?)\\]", ip);
-
-            Files.write(Paths.get("/etc/nginx/conf.d/"), content.getBytes(charset));
-
+            for (String s : templates) {
+                System.out.println(s);
+                Path path = Paths.get(s);
+                Charset charset = StandardCharsets.UTF_8;
+                String content = new String(Files.readAllBytes(path), charset);
+                content = content.replaceAll("\\[(.*?)\\]", ip);
+                Files.write(Paths.get("/etc/nginx/conf.d/".concat("")), content.getBytes(charset));
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
